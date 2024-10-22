0a4521f2f7fa1c94936f8cdd01aac8d1254c8c19
lech-qr <lech@jet-it.pl>
2024-10-22 12:16:30 +0200

    Publikacja

--- page.php
+++ page.php
@@ -1,10 +1,17 @@
 <?php get_header(); ?>
-  <main class="container">
-    <article class="row justify-content-center">
-      <div class="col-xs-12 col-lg-6">
-          <h2><?php the_title(); ?></h2>
-          <?php the_content(); ?>
+<main class="container">
+  <article class="row justify-content-center">
+    <div class="col-xs-12 col-lg-6">
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>   
+    </div>
+    <?php if (has_post_thumbnail($post_id)) { ?> 
+      <div class="col-xs-12 col-lg-6 text-end">
+        <?php echo get_the_post_thumbnail($post_id, 'large'); ?>
       </div>
-    </article>
-  </main>
+    <?php  wp_reset_postdata();
+    }      
+    ?>    
+  </article>
+</main>
 <?php get_footer(); ?>
